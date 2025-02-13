9603cafef452d25fd1a798ca8d54a320b38228e8
T-ayy-lor <165335817+T-ayy-lor@users.noreply.github.com>
2025-02-13 13:04:28 -0500

    added faceMesh

--- handPose/sketch.js
+++ handPose/sketch.js
@@ -1,38 +1,38 @@
 let video;
-let handPose;
-let hands = [];
-let connections;
+let faceMesh;
+let faces = [];
+let triangles;
 
 function preload() {
-  handPose = ml5.handPose();
+  faceMesh = ml5.handPose();
 }
 
-function gotHands(results) {
-  hands = results;
+function gotFaces(results) {
+  faces = results;
 }
 
 function mousePressed() {
-  console.log(hands);
+  console.log(faces);
 }
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.hide();
-  handPose.detectStart(video, gotHands);
-  connections = handPose.getConnections();
+  faceMesh.detectStart(video, gotFaces);
+  triangles = faceMesh.getConnections();
 }
 
 function draw() {
   background(220);
   image(video, 0, 0);
 
-  if (hands.length > 0) {
-    for (let hand of hands) {
+  if (faces.length > 0) {
+    for (let hand of faces) {
       if (hand.confidence > 0.1) {
-        for (let i = 0; i < connections.length; i++) {
-          let pointAIndex = connections[i][0];
-          let pointBIndex = connections[i][1];
+        for (let i = 0; i < triangles.length; i++) {
+          let pointAIndex = triangles[i][0];
+          let pointBIndex = triangles[i][1];
           let pointA = hand.keypoints[pointAIndex];
           let pointB = hand.keypoints[pointBIndex];
           stroke(0, 255, 0);
@@ -42,7 +42,7 @@ function draw() {
       }
     }
 
-    for (let hand of hands) {
+    for (let hand of faces) {
       if (hand.confidence > 0.1) {
         for (let i = 0; i < hand.keypoints.length; i++) {
           let keypoint = hand.keypoints[i];
